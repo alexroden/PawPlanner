80bf9a329bd88c679b12ce3835e13c8f942f04ba
Alexander Broom-Roden <b.r_alex@hotmail.co.uk>
2017-12-15 16:49:14 +0000

    Added models, and started the structure of migrations

---
+++ database/migrations/2017_12_15_162403_CreatePetsTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->index();
+            $table->string('name');
+            $table->integer('type_id')->index();
+            $table->string('breed')->nullable()->default(null);
+            $table->text('medical')->nullable()->default(null);
+            $table->text('info')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
---
+++ database/migrations/2017_12_15_162411_CreateAppointmentsTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->integer('pet_id')->index();
+            $table->timestamp('scheduled_start_at');
+            $table->timestamp('scheduled_end_at');
+            $table->timestamp('actual_start_at')->nullable()->default(null);
+            $table->timestamp('actual_end_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
